f541e1a66cfdeb0ad45672b562c2b1f2e4cc760e
frank.huang <2489868503@qq.com>
2023-02-22 22:28:26 +0800

    fix: 修复ElLoading报错问题

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -1,16 +1,16 @@
-import { ElLoading } from 'element-plus'
-import {
-    Loading
-} from '@element-plus/icons-vue'
+import {ElLoading} from 'element-plus'
+
 let loading = null
 
-export const openLoading = (options={})=>{
-    const text = options.text||'加载中'
+export const openLoading = (options = {
+    text: "加载中"
+}) => {
+    const text = options.text
     loading = ElLoading.service({
         lock: true,
         text: text,
     })
 }
 export const closeLoading = () => {
-    loading&&loading.close()
+    loading && loading.close()
 }
