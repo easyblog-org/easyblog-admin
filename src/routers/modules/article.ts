06f06abc45f5e450aab72153653ad8b7dbc24d36
huangxin <2489868503@qq.com>
2023-09-06 22:42:36 +0800

    fix: 文章详情页开发

--- src/routers/modules/article.ts
+++ src/routers/modules/article.ts
@@ -13,17 +13,24 @@ const articleRouter = [{
     },
     children: [
         {
-            path: 'article',
-            component: () => import('@/views/article/article/index.vue'),
+            path: 'list',
+            component: () => import('@/views/article/list/index.vue'),
             name: 'article',
-            meta: { title: '文章管理' , icon: 'Document'}
+            meta: { title: '文章管理' , icon: 'Management'}
         },
         {
             path: 'category',
             component: () => import('@/views/article/category/index.vue'),
             name: 'category',
             meta: { title: '文章分类' , icon: 'Collection'}
-        }
+        },
+        {
+            path: 'detail',
+            name: 'detail',
+            component: () => import('@/views/article/detail/index.vue'),
+            meta: { title: '文章详情' , icon: 'Document'},
+            hidden: true
+        },
     ]
 }]
 
